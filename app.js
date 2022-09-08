9756d381fe6ed6ce30d0a14d5d62c1d84b414a1c
IgnaBarrenechea <ibarrenechea79@gmail.com>
2022-09-08 00:21:32 -0300

    Se creo el archivo ´Procfile´ para poder subirlo a Heroku

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.get("/",(req,res) => {
     res.sendFile(path.join(__dirname + "/views/home.html"))
 })
 
-app.listen(3000,function(){
-    console.log("servidor corriendo")
+app.listen(process.env.PORT || 3000, function() {
+    console.log("Servidor corriendo");
 })
 
